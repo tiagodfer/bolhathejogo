0037b9c2f72f2b92f472b23725f7baa6ee4c154c
tiagodiasferreira <51669608+tiagodiasferreira@users.noreply.github.com>
2019-06-23 20:41:19 -0300

    Update bolhathejogo.rkt

--- bolhathejogo.rkt
+++ bolhathejogo.rkt
@@ -1,66 +1,88 @@
-;; The first three lines of this file were inserted by DrRacket. They record metadata
-;; about the language level of this file in a form that our tools can easily process.
-#reader(lib "htdp-beginner-reader.ss" "lang")((modname |20190626|) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
 (require 2htdp/image)
 (require 2htdp/universe)
 
 (define ALTURA 400)
 (define LARGURA 400)
 
-;; um bola
-;; (make-bola (x y ix iy r c))
-(define-struct bola (x y ix iy c r))
+;; um elemento bolha de Bolha eh um :
+;; (make-bolha (x y ix iy r c d)), onde :
+;; - x : Number, coordenada X
+;; - y : Number, coordenada Y
+;; - ix : Number, incrementos na coordenada X
+;; - iy : Number, incrementos na coordenada Y
+;; - r : Number, raio de bolha
+;; - c : Symbol, cor de bolha
+;; - d : Boolean, true se duro, false se mole
+(define-struct bolha (x y ix iy cor raio duro))
 
-(define B1 (make-bola 0 0 1 1 "blue" 10))
+(define B1 (make-bolha 0 0 1 1 'blue 10 true))
 
-;; um quadrado
-;; (make-quadrado (x y c l))
+;; um elemento quadrado de Quadrado eh um :
+;; (make-quadrado (x y c l)), onde :
+;; - x : Number, coordenada X
+;; - y : Number, coordenada Y
+;; - c : Symbol, cor de quadrado
+;; - l : Number, lado de quadrado
 (define-struct quadrado (x y c l))
 
-(define Q1 (make-quadrado 100 100 "pink" 10))
+(define Q1 (make-quadrado 100 100 'pink 10))
 
 
-;; desenha-b
-(define (desenha-b b)
+;; um Cena eh :
+;; - ou empty
+;; - ou (cons bolha quadrado), onde :
+;;     - bolha : Bolha
+;;     - quadrado : Quadrado
+
+
+;; desenha-bolha : Bolha -> Cena
+(define (desenha-bolha b)
   (place-image
-   (circle (bola-r b) "solid" (bola-c b)) (bola-x b) (bola-y b) (empty-scene ALTURA LARGURA)))
+   (circle (bolha-raio b) 'solid (bolha-cor b)) (bolha-x b) (bolha-y b) (empty-scene ALTURA LARGURA)))
 
-;; desenha-q
-(define (desenha-q q)
+
+;; desenha-quadrado : Quadrado -> Cena
+(define (desenha-quadrado q)
   (place-image
-   (square (quadrado-l q) "solid" (quadrado-c q)) (quadrado-x q) (quadrado-y q)
+   (square (quadrado-l q) 'solid (quadrado-c q)) (quadrado-x q) (quadrado-y q)
    (empty-scene ALTURA LARGURA)))
 
-;; fora
+
+;; fora : Bolha -> Boolean
 (define (fora b)
-  (not (and (<= 0 (bola-x b) LARGURA) (<= 0 (bola-y b) ALTURA))))
+  (not (and (<= 0 (bolha-x b) LARGURA) (<= 0 (bolha-y b) ALTURA))))
 
-;; bateu
+
+;; bateu : Bolha Quadrado -> Boolean
 (define (bateu b q)
-  (and (= (bola-x b) (quadrado-x q)) (= (bola-y b) (quadrado-y q))))
+  (and (= (bolha-x b) (quadrado-x q)) (= (bolha-y b) (quadrado-y q))))
 
-;; move
+
+;; move : Bolha -> Bolha
 (define (move b)
-  (make-bola (+ (bola-x b) (bola-ix b)) (+ (bola-y b) (bola-iy b)) (bola-ix b) (bola-iy b) (bola-c b)
-             (bola-r b)))
+  (make-bolha (+ (bolha-x b) (bolha-ix b)) (+ (bolha-y b) (bolha-iy b)) (bolha-ix b) (bolha-iy b) (bolha-cor b)
+             (bolha-raio b) (bolha-duro b)))
 
-;; move-ate
+;; move-ate : Bolha Quadrado -> Cena
 (define (move-ate b q)
   (cond
     [(fora b) empty]
-    [(bateu b q) (cons (desenha-b (make-bola (quadrado-x q)
+    [(bateu b q) (cons (desenha-bolha (make-bolha (quadrado-x q)
                                              (quadrado-y q)
-                                             (- (bola-ix b))
-                                             (- (bola-iy b))
-                                             (bola-c b)
-                                             (bola-r b)))
-                       (cons (desenha-q q)(move-ate (move (make-bola (quadrado-x q)
+                                             (- (bolha-ix b))
+                                             (- (bolha-iy b))
+                                             (bolha-cor b)
+                                             (bolha-raio b)
+                                             (bolha-duro b)))
+                       (cons (desenha-quadrado q)(move-ate (move (make-bolha (quadrado-x q)
                                                                      (quadrado-y q)
-                                                                     (- (bola-ix b))
-                                                                     (- (bola-iy b))
-                                                                     (bola-c b)
-                                                                     (bola-r b))) q)))]
-    [else (cons (desenha-b b) (cons (desenha-q q)(move-ate (move b) q)))]))
+                                                                     (- (bolha-ix b))
+                                                                     (- (bolha-iy b))
+                                                                     (bolha-cor b)
+                                                                     (bolha-raio b)
+                                                                     (bolha-duro b))) q)))]
+    [else (cons (desenha-bolha b) (cons (desenha-quadrado q)(move-ate (move b) q)))]))
+
 
 ;; run
 (run-movie 0.001 (move-ate B1 Q1))
